c921074fef1150da051c7e22624516f03675b1a4
Art.OCathain <Art.OCathain@dsit.gov.uk>
2025-02-28 13:37:06 +0000

    big test

--- tests/proxmoxsandboxtest/test_proxmox_sandbox_lifecycle.py
+++ tests/proxmoxsandboxtest/test_proxmox_sandbox_lifecycle.py
@@ -1,18 +1,18 @@
 from pathlib import Path
-from proxmoxsandbox.inspect.proxmox.async_proxmox import AsyncProxmoxAPI
-from proxmoxsandbox.inspect.proxmox.infra_commands import InfraCommands
-from proxmoxsandbox.inspect.schema import (
-    ProxmoxSandboxEnvironmentConfig,
-    VmConfig,
-    VmSourceConfig,
-)
+
 from proxmoxsandboxtest.proxmox_sandbox_utils import (
     setup_requests_logging,
     setup_sandbox,
 )
 
+from proxmoxsandbox.inspect.proxmox.async_proxmox import AsyncProxmoxAPI
+from proxmoxsandbox.inspect.proxmox.infra_commands import InfraCommands
 from proxmoxsandbox.inspect.proxmox_sandbox_environment import ProxmoxSandboxEnvironment
-
+from proxmoxsandbox.inspect.schema import (
+    ProxmoxSandboxEnvironmentConfig,
+    VmConfig,
+    VmSourceConfig,
+)
 
 CURRENT_DIR = Path(__file__).parent
 
@@ -194,3 +194,58 @@ async def test_ova() -> None:
             environments=envs_dict,
             interrupted=False,
         )
+
+
+async def test_everything(proxmox_api) -> None:
+    envs_dict = {}
+
+    sdn_config = await InfraCommands(proxmox_api, node="proxmox").generate_sdn_config(
+        aliases=("alias1", "alias2", None)
+    )
+
+    sandbox_env_config = ProxmoxSandboxEnvironmentConfig(
+        vms_config=(
+            VmConfig(
+                vm_source_config=VmSourceConfig(built_in="ubuntu24.04"),
+                vnet_aliases=("alias1", "alias2"),
+            ),
+            VmConfig(
+                vm_source_config=VmSourceConfig(built_in="kali"),
+                vnet_aliases=("alias1",),
+            ),
+            VmConfig(
+                vm_source_config=VmSourceConfig(
+                    ova=Path("./tests/oVirtTinyCore64-13.11.ova")
+                ),
+                vnet_aliases=("alias1", "alias2"),
+                is_sandbox=True,
+            ),
+            VmConfig(
+                vm_source_config=VmSourceConfig(
+                    ova=Path("./tests/oVirtTinyCore64-13.11.ova")
+                ),
+                is_sandbox=False,
+            ),
+        ),
+        sdn_config=sdn_config,
+    )
+    try:
+        task_name, envs_dict = await setup_sandbox("tcova", sandbox_env_config)
+        uname_result = await envs_dict["default"].exec(
+            [
+                "uname",
+                "-a",
+            ]
+        )
+        assert uname_result.success, f"Failed to run uname: {uname_result=}"
+        assert "ubuntu" in uname_result.stdout, (
+            f"Unexpected result of uname: {uname_result.stdout=}"
+        )
+    finally:
+        pass
+        # await ProxmoxSandboxEnvironment.sample_cleanup(
+        #     task_name="unused",
+        #     config=sandbox_env_config,
+        #     environments=envs_dict,
+        #     interrupted=False,
+        # )
