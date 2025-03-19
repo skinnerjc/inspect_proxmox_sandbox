b1592a6715fed2b7524c2cb33dad12754abd84c1
Art.OCathain <Art.OCathain@dsit.gov.uk>
2025-03-19 11:54:28 +0000

    failing test for eval

---
+++ tests/proxmoxsandboxtest/test_eval.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+
+from inspect_ai import Task, eval, task
+from inspect_ai.dataset import Sample
+from inspect_ai.model import ModelOutput, get_model
+from inspect_ai.scorer import includes
+from inspect_ai.solver import basic_agent
+from inspect_ai.tool import bash
+from inspect_ai.util import SandboxEnvironmentSpec
+
+from proxmoxsandbox.inspect.proxmox_sandbox_environment import (
+    ProxmoxSandboxEnvironmentConfig,
+)
+
+
+CURRENT_DIR = Path(__file__).parent
+
+
+@task
+def task_for_test() -> Task:
+    return Task(
+        dataset=[
+            Sample(
+                input="""
+sample text
+        """,
+                target="42",
+            ),
+        ],
+        solver=[
+            basic_agent(
+                tools=[bash()],
+                message_limit=20,
+            ),
+        ],
+        scorer=includes(),
+        sandbox=SandboxEnvironmentSpec(
+            type="proxmox",
+            config=ProxmoxSandboxEnvironmentConfig(),
+        ),
+    )
+
+
+def test_inspect_eval() -> None:
+    eval_logs = eval(
+        tasks=[task_for_test()],
+        model=get_model(
+            "mockllm/model",
+            custom_outputs=[
+                ModelOutput.for_tool_call(
+                    model="mockllm/model",
+                    tool_name="bash",
+                    tool_arguments={"cmd": "uname -a"},
+                ),
+                ModelOutput.for_tool_call(
+                    model="mockllm/model",
+                    tool_name="submit",
+                    tool_arguments={"answer": "42"},
+                ),
+            ],
+        ),
+        log_level="trace",
+        # sandbox_cleanup=False
+    )
+
+    assert len(eval_logs) == 1
