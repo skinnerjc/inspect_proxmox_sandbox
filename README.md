f0b268331c0375c9419bd089b04398c848d30ffa
Art.OCathain <Art.OCathain@dsit.gov.uk>
2025-03-31 08:33:38 +0000

    Improve convert_ova script

--- README.md
+++ README.md
@@ -179,8 +179,8 @@ Instead:
 Proxmox supports OVA import but not OVA export. It is possible to extract the disk images of VMs 
 from a Proxmox server in qcow2 format (instructions for this can be found online).
 
-Once you have the disk images locally, you can use the convenience script `scripts/convert_ova.sh`
-to convert it into an OVA. Note, it assumes you have VirtualBox installed.
+Once you have the disk images locally, you can use the convenience script `scripts/ova/convert_ova.sh`
+to convert it into an OVA.
 
 ## Observing the VMs
 
--- scripts/ova/converter_script.sh
+++ scripts/ova/converter_script.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
-# note: the uefi param doesn't actually work in proxmox, though it seems fine in VirtualBox
-# this script has problems with idempotency
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <source_directory> [uefi]"
-    echo "Add 'uefi' as second parameter to enable UEFI boot mode"
-    exit 1
-fi
-
-SOURCE_DIR="$1"
-TEMP_DIR="/tmp/qcow2_to_ova_conversion"
-OUTPUT_DIR="$SOURCE_DIR/converted_ovas"
-UEFI_MODE=0
-
-# Check if UEFI mode is requested
-if [ "$2" = "uefi" ]; then
-    UEFI_MODE=1
-    echo "UEFI boot mode enabled"
-fi
-
+# This script runs inside a Docker container, so don't try to run it yourself
 set -eu
 
-mkdir -p "$TEMP_DIR" "$OUTPUT_DIR"
+SOURCE_DIR="/source"
+OUTPUT_DIR="/output"
+TEMP_DIR="/tmp/qcow2_to_ova_conversion"
+UEFI_MODE=$1
+
+mkdir -p "$TEMP_DIR"
 
 for qcow2_file in "$SOURCE_DIR"/*.qcow2; do
     if [ ! -f "$qcow2_file" ]; then
@@ -32,31 +18,19 @@ for qcow2_file in "$SOURCE_DIR"/*.qcow2; do
     filename=$(basename "$qcow2_file" .qcow2)
     echo "Processing: $filename"
 
-    # Convert qcow2 to VDI
     qemu-img convert -f qcow2 -O vdi "$qcow2_file" "$TEMP_DIR/$filename.vdi"
-
-    # Create and configure VM
     VBoxManage createvm --name "$filename" --ostype Linux26_64 --register
-    
-    # Configure basic VM settings
     VBoxManage modifyvm "$filename" --memory 2048 --cpus 2 --acpi on --boot1 disk
-    
-    # Configure UEFI if requested
     if [ $UEFI_MODE -eq 1 ]; then
         VBoxManage modifyvm "$filename" --firmware efi
     fi
     
-    # Configure storage
     VBoxManage storagectl "$filename" --name "SATA Controller" --add sata --controller IntelAhci
     VBoxManage storageattach "$filename" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$TEMP_DIR/$filename.vdi"
-
-    # Export to OVA
     VBoxManage export "$filename" --output "$OUTPUT_DIR/$filename.ova"
-
-    # Cleanup - VBoxManage unregistervm --delete also removes the VDI
     VBoxManage unregistervm "$filename" --delete
 
-    echo "Converted: $OUTPUT_DIR/$filename.ova"
+    echo "Converted: $filename.ova"
 done
 
 rm -rf "$TEMP_DIR"
